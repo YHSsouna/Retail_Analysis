ab856384d4c5587a7c22199987f51011dd7ead77
YHSsouna <yassinehsouna@yahoo.com>
2025-07-02 16:30:28 +0100

    Add dags Python files despite gitignore

---
+++ dags/auchan_dag.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+# Default arguments for DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 2, 24),
+    'retries': 1,
+}
+
+# Define DAG
+dag = DAG(
+    'auchan_scraper',
+    default_args=default_args,
+    description='DAG to scrape data from Auchan',
+    schedule_interval='@daily',  # Runs daily
+)
+
+# Scraping task using BashOperator
+scrape_auchan = BashOperator(
+    task_id='scrape_auchan',
+    bash_command='python /opt/airflow/web_scraping_scripts/auchan.py',
+    dag=dag,
+)
+
+categorize_auchan = BashOperator(
+    task_id='categorize_auchan',
+    bash_command='python /opt/airflow/web_scraping_scripts/auchan_cat.py',
+    dag=dag,
+)
+
+section_auchan = BashOperator(
+    task_id='section_auchan',
+    bash_command='python /opt/airflow/web_scraping_scripts/auchan_section.py',
+    dag=dag,
+)
+
+normalize_auchan = BashOperator(
+    task_id='normalize_auchan',
+    bash_command='python /opt/airflow/web_scraping_scripts/auchan_norm.py',
+    dag=dag,
+)
+
+
+scrape_auchan >> section_auchan >> categorize_auchan
+scrape_auchan >> normalize_auchan
