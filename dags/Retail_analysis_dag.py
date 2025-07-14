38924589d1e8cf3deaee5cc3d09db28b2db8dc2c
YHSsouna <yassinehsouna@yahoo.com>
2025-07-14 18:38:47 +0100

    updating dags

--- dags/Retail_analysis_dag.py
+++ dags/Retail_analysis_dag.py
@@ -27,24 +27,10 @@ with DAG('all_scrapers_combined',
         section = BashOperator(task_id='section', bash_command='python /opt/airflow/web_scraping_scripts/auchan_section.py')
         categorize = BashOperator(task_id='categorize', bash_command='python /opt/airflow/web_scraping_scripts/auchan_cat.py')
         normalize = BashOperator(task_id='normalize', bash_command='python /opt/airflow/web_scraping_scripts/auchan_norm.py')
-        stg_auchan = BashOperator(
-            task_id='staging_auchan',
-            bash_command="""
-                    pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
-                    dbt run --select stg_auchan --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
-                """,
-        )
 
-        inter_auchan = BashOperator(
-            task_id='intermediate_auchan',
-            bash_command="""
-                    pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
-                    dbt run --select inter_auchan --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
-                """,
-        )
 
-        scrape >> section >> categorize >> stg_auchan >> inter_auchan
-        scrape >> normalize >> stg_auchan >> inter_auchan
+        scrape >> section >> categorize
+        scrape >> normalize
 
     # Carrefour pipeline
     with TaskGroup("carrefour_pipeline") as carrefour:
@@ -52,22 +38,9 @@ with DAG('all_scrapers_combined',
         section = BashOperator(task_id='section', bash_command='python /opt/airflow/web_scraping_scripts/carrefour_section.py')
         categorize = BashOperator(task_id='categorize', bash_command='python /opt/airflow/web_scraping_scripts/carrefour_cat.py')
 
-        stg_carrefour = BashOperator(
-            task_id='staging_carrefour',
-            bash_command="""
-                    pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
-                    dbt run --select stg_carrefour --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
-                """,
-        )
-        inter_carrefour = BashOperator(
-            task_id='Intermediate_Carrefour',
-            bash_command="""
-                    pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
-                    dbt run --select inter_carrefour --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
-                """,
-        )
 
-        scrape >> section >> categorize >> stg_carrefour >> inter_carrefour
+
+        scrape >> section >> categorize
 
     # Géant pipeline
     with TaskGroup("biocoop_pipeline") as biocoop:
@@ -77,23 +50,9 @@ with DAG('all_scrapers_combined',
         categorize = BashOperator(task_id='categorize',
                                   bash_command='python /opt/airflow/web_scraping_scripts/biocoop_cat.py')
 
-        stg_biocoop = BashOperator(
-            task_id='staging_biocoop',
-            bash_command="""
-                    pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
-                    dbt run --select stg_biocoop --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
-                """,
-        )
 
-        inter_biocoop = BashOperator(
-            task_id='Intermediate_biocoop',
-            bash_command="""
-                    pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
-                    dbt run --select inter_biocoop --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
-                """,
-        )
 
-        scrape >> section >> categorize >> stg_biocoop >> inter_biocoop
+        scrape >> section >> categorize
 
     # Monoprix pipeline
     with TaskGroup("labellevie_pipeline") as labellevie:
@@ -101,23 +60,10 @@ with DAG('all_scrapers_combined',
         section = BashOperator(task_id='section', bash_command='python /opt/airflow/web_scraping_scripts/labellevie_section.py')
         categorize = BashOperator(task_id='categorize', bash_command='python /opt/airflow/web_scraping_scripts/labellevie_cat.py')
         normalize = BashOperator(task_id='normalize', bash_command='python /opt/airflow/web_scraping_scripts/labellevie_norm.py')
-        stg_labellevie = BashOperator(
-            task_id='staging_labellevie',
-            bash_command="""
-                            pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
-                            dbt run --select stg_labellevie --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
-                        """,
-        )
-        inter_labellevie = BashOperator(
-            task_id='Intermediate_Labellevie',
-            bash_command="""
-                            pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
-                            dbt run --select inter_labellevie --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
-                        """,
-        )
 
-        scrape >> section >> categorize >> stg_labellevie >> inter_labellevie
-        scrape >> normalize >> stg_labellevie >> inter_labellevie
+
+        scrape >> section >> categorize
+        scrape >> normalize
 
 
     with TaskGroup("ML_pipeline") as ML_pipeline:
@@ -126,13 +72,112 @@ with DAG('all_scrapers_combined',
         predictions = BashOperator(task_id='predictions',bash_command='python /opt/airflow/web_scraping_scripts/predictions.py')
         run_if_sunday >> ml_training >> predictions
 
-    Data_marts = BashOperator(
-            task_id='Data_Warehouse_modeling',
+    with TaskGroup("DBT_Models") as DBT_pipeline:
+        stg_auchan = BashOperator(
+            task_id='staging_auchan',
             bash_command="""
-                pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
-                dbt run --select marts --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
-            """,
+                    pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
+                    dbt run --select stg_auchan --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                """,
+            trigger_rule='all_done'
         )
+
+        inter_auchan = BashOperator(
+            task_id='intermediate_auchan',
+            bash_command="""
+                    pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
+                    dbt run --select inter_auchan --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                """,
+        )
+        stg_carrefour = BashOperator(
+            task_id='staging_carrefour',
+            bash_command="""
+                    pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
+                    dbt run --select stg_carrefour --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                """,
+            trigger_rule='all_done'
+        )
+        inter_carrefour = BashOperator(
+            task_id='Intermediate_Carrefour',
+            bash_command="""
+                    pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
+                    dbt run --select inter_carrefour --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                """,
+        )
+        stg_biocoop = BashOperator(
+            task_id='staging_biocoop',
+            bash_command="""
+                            pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
+                            dbt run --select stg_biocoop --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                        """,
+            trigger_rule='all_done'
+        )
+
+        inter_biocoop = BashOperator(
+            task_id='Intermediate_biocoop',
+            bash_command="""
+                            pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
+                            dbt run --select inter_biocoop --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                        """,
+        )
+        stg_labellevie = BashOperator(
+            task_id='staging_labellevie',
+            bash_command="""
+                                    pip install dbt-core==1.5.1 dbt-postgres==1.5.1 &&
+                                    dbt run --select stg_labellevie --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                                """,
+            trigger_rule='all_done'
+        )
+        inter_labellevie = BashOperator(
+            task_id='Intermediate_Labellevie',
+            bash_command="""
+                                    dbt run --select inter_labellevie --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                                """,
+        )
+        dim_date = BashOperator(
+            task_id='dim_date',
+            bash_command="""
+                                    dbt run --select dim_date --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                                """,
+        )
+        dim_categories = BashOperator(
+            task_id='dim_categories',
+            bash_command="""
+                                    dbt run --select dim_category --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                                """,
+        )
+        dim_product = BashOperator(
+            task_id='dim_product',
+            bash_command="""
+                                    dbt run --select dim_product --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                                """,
+        )
+        dim_picture = BashOperator(
+            task_id='dim_picture',
+            bash_command="""
+                                    dbt run --select dim_picture --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                                """,
+        )
+        dim_predictions = BashOperator(
+            task_id='dim_predictions',
+            bash_command="""
+                                    dbt run --select dim_predictions --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                                """,
+        )
+        dim_store = BashOperator(
+            task_id='dim_store',
+            bash_command="""
+                                    dbt run --select dim_store --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                                """,
+        )
+        fact_sales = BashOperator(
+            task_id='fact_sales',
+            bash_command="""
+                                            dbt run --select fact_sales --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
+                                        """,
+        )
+
+
 
     Test = BashOperator(
             task_id='Data_Quality_Tests',
@@ -140,10 +185,14 @@ with DAG('all_scrapers_combined',
                 dbt test --project-dir /opt/airflow/retail/retail --profiles-dir /opt/airflow/retail
             """,
         )
+    stg_auchan >> inter_auchan >> dim_date >> dim_categories >> dim_picture >>  dim_predictions >> dim_store >> dim_product >> fact_sales
+    stg_labellevie >> inter_labellevie >> dim_date >> dim_categories >> dim_picture >>  dim_predictions >> dim_store >> dim_product >> fact_sales
+    stg_biocoop >> inter_biocoop >> dim_date >> dim_categories >> dim_picture >>  dim_predictions >> dim_store >> dim_product >> fact_sales
+    stg_carrefour >> inter_carrefour >> dim_date >> dim_categories >> dim_picture >>  dim_predictions >> dim_store >> dim_product >> fact_sales
 
 
 
 
 
     # Set dependencies — run ETL after all TaskGroups complete
-    [auchan, carrefour, labellevie, biocoop, ML_pipeline] >> Data_marts >> Test
+    [auchan, carrefour, labellevie, biocoop, ML_pipeline] >> DBT_pipeline >> Test
